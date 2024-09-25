9666a4895c48ae37fed0db753e6c76ceb96d639e
Nayem Uddin <nayemddincucse@gmail.com>
2024-09-25 21:25:02 +0600

    TYpe casting learning

---
+++ src/TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        /*
+         * Type Casting
+         * there is two type of type casting
+         * Implicit Type Casting & Explicit Type Castin
+         *
+         */
+        System.out.println("Welcome to type casting lesson");
+
+
+
+    }
+}
