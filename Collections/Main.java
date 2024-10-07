2cec4da947c5502ea6ac436317b6048250309087
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-07 22:59:32 +0600

    Core concept about Collection Framework

--- Collections/Main.java
+++ Collections/Main.java
@@ -3,6 +3,9 @@ package Collections;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Let\'s recap collection framework");
-        
+
+        // 9 key interface in Collecition frameworks
+        // Collection, List, Set, SortedSet, NavigableSet, Queue, Map, SortedMap,
+        // NavigableMap
     }
 }
