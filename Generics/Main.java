1a01d06d01ec7fe88758eaaf173bab7e436d5f30
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-12 10:20:13 +0600

    generics implemented

--- Generics/Main.java
+++ Generics/Main.java
@@ -4,25 +4,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Learn Generics");
 
-        Box box = new Box();
+        // Box box = new Box();
 
-        box.setValue(10);
+        // box.setValue(10);
 
         // this is not type safe now
-        int val = (Integer) box.getValue();
-        System.out.println(val);
+        // int val = (Integer) box.getValue();
+        // System.out.println(val);
 
-        box.setValue("box");
+        // box.setValue("box");
 
         // Not type safe
-        String str = box.getValue();
+        // String str = box.getValue();
 
         // Type cast
-        String str1 = (String) box.getValue();
-        // System.out.println(str1);
-        System.out.println(str);
+        // String str1 = (String) box.getValue();
+        // // System.out.println(str1);
+        // System.out.println(str);
 
         // we need to manual type casting generics help to solve this issue
+        Box<Integer> box1 = new Box<>();
 
+        box1.setValue(10);
+        System.out.println(box1.getValue());
+
+        Box<String> boxstr = new Box<>();
+
+        boxstr.setValue("10");
+        System.out.println(boxstr.getValue());
     }
 }
