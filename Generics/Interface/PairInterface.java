cc5078ac323d1ecec544f75911a247cce1abf197
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-12 14:32:24 +0600

    PaiInterface initialize

---
+++ Generics/Interface/PairInterface.java
@@ -0,0 +1,18 @@
+package Generics.Impl;
+
+public interface PairInterface<K, V> {
+    void set(K key, V value);
+
+    // Get the current key
+    K getKey();
+
+    // Get the key of given value
+    K getKey(V value);
+
+    // get current value
+    V getValue();
+
+    // Get the value of given key
+    V getValue(K key);
+
+}
