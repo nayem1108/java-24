28ce9d6a74c5f32358ab866090563ade98d1b9de
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-13 11:41:07 +0600

    Two dimentional array initialization

---
+++ DS/Array/Array2D.java
@@ -0,0 +1,100 @@
+package DS.Array;
+
+public class Array2D {
+    public static void main(String[] args) {
+        // Boolean Array
+        boolean[][] arrboolean = new boolean[4][];
+        boolean[][] arrboolean1 = { { true, false }, { false, true } };
+
+        for (int i = 0; i < arrboolean1.length; i++) {
+            for (int j = 0; j < arrboolean1[i].length; j++) {
+                System.out.print(arrboolean1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Character Array
+        char[][] arrchar = new char[4][];
+        char[][] arrchar1 = { { 'a', 'b' }, { 'c', 'd' } };
+
+        for (int i = 0; i < arrchar1.length; i++) {
+            for (int j = 0; j < arrchar1[i].length; j++) {
+                System.out.print(arrchar1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Byte Array
+        byte[][] arrbyte = new byte[4][];
+        byte[][] arrbyte1 = { { 1, 3 }, { 4, 2 } };
+
+        for (int i = 0; i < arrbyte1.length; i++) {
+            for (int j = 0; j < arrbyte1[i].length; j++) {
+                System.out.print(arrbyte1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Int Array
+        int[][] arrInt = new int[4][];
+        int[][] arrInt1 = { { 1, 3 }, { 4, 2 } };
+
+        for (int i = 0; i < arrInt1.length; i++) {
+            for (int j = 0; j < arrInt1[i].length; j++) {
+                System.out.print(arrInt1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // Long Array
+        long[][] arrlong = new long[4][];
+        long[][] arrlong1 = { { 1, 3 }, { 4, 2 } };
+
+        for (int i = 0; i < arrlong1.length; i++) {
+            for (int j = 0; j < arrlong1[i].length; j++) {
+                System.out.print(arrlong1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // float Array
+        float[][] arrfloat = new float[4][];
+        float[][] arrfloat1 = { { 1, 3 }, { 4, 2 } };
+
+        for (int i = 0; i < arrfloat1.length; i++) {
+            for (int j = 0; j < arrfloat1[i].length; j++) {
+                System.out.print(arrfloat1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // double Array
+        double[][] arrdouble = new double[4][];
+        double[][] arrdouble1 = { { 1, 3 }, { 4, 2 } };
+
+        for (int i = 0; i < arrdouble1.length; i++) {
+            for (int j = 0; j < arrdouble1[i].length; j++) {
+                System.out.print(arrdouble1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // String Array
+        String[][] arrString = new String[4][];
+        String[][] arrString1 = { { "Nayem", "Uddin" }, { "Fahim", "Ahmed" } };
+
+        for (int i = 0; i < arrString1.length; i++) {
+            for (int j = 0; j < arrString1[i].length; j++) {
+                System.out.print(arrString1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
