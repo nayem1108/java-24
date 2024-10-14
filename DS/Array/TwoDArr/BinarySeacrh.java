a70368d02a79ccf4ead84cd100adad52503fbd07
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-14 09:16:32 +0600

    binary search

--- DS/Array/TwoDArr/BinarySeacrh.java
+++ DS/Array/TwoDArr/BinarySeacrh.java
@@ -1,5 +1,14 @@
 package DS.Array.TwoDArr;
 
-public class BinarySeacrh {
+import java.util.Scanner;
 
+public class BinarySeacrh {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        int target = inputScanner.nextInt();
+
+        int arr[][] = { { 1, 4 }, { 3, 5 }, { 6, 7 }, { 8, 9 }, { 12, 11 } };
+
+        inputScanner.close();
+    }
 }
