3bd2ced7c13a26f13de0ab3f83719c650c87c2bc
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-16 21:44:11 +0600

    print Hello World 5 times

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -2,11 +2,63 @@ package Recursion;
 
 public class Main {
     public static void main(String[] args) {
-        // Pring 5 times hello world
+        // Print 5 times hello world
+
+        /*
+         * There are some ways
+         * Either call this message() 5 times
+         * message();
+         * message();
+         * message();
+         * message();
+         * message();
+         */
+
+        /*
+         * Or
+         * message();
+         */
+
+        /*
+         * Or
+         */
         message();
     }
 
     static void message() {
+        /*
+         * Or
+         * System.out.println("Hello World");
+         * System.out.println("Hello World");
+         * System.out.println("Hello World");
+         * System.out.println("Hello World");
+         * System.out.println("Hello World");
+         */
+
+        /*
+         * Or
+         */
         System.out.println("Hello World");
+
+        message2();
+        message3();
+        message4();
+        message5();
+    }
+
+    static void message2() {
+        System.out.println("Hello World!");
+    }
+
+    static void message3() {
+        System.out.println("Hello World!");
+    }
+
+    static void message4() {
+        System.out.println("Hello World!");
+    }
+
+    static void message5() {
+        System.out.println("Hello World!");
     }
 }
