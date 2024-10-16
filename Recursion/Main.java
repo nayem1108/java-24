b56883fe86b8be76129f4655c9bfc8327ed43b75
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-16 21:38:04 +0600

    learn recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Main {
+    public static void main(String[] args) {
+        // Pring 5 times hello world
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello World");
+    }
+}
