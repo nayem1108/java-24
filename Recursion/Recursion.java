8749eaa2b681b662156e922609359113050b58f6
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-17 09:17:12 +0600

    recursion with theory

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -2,12 +2,12 @@ package Recursion;
 
 public class Recursion {
     /*
-     * 
      * Recursion is a concept when a function is calling it self is called recursion
      * 
      * There is two point to archive recursion
      * 1. Base Condtion : where my recursion call will be end
      * 2. Untill aechive the best condition then call the function
+     * 3. Method Body
      */
     public static void main(String[] args) {
         // We calling a function which will print 1,2,3,4,5
@@ -15,18 +15,26 @@ public class Recursion {
     }
 
     static void print(int n) {
+
+        /*
+         * Body
+         */
         System.out.println(n);
 
-        // When n is 5 then stop call the function
         /*
          * Base condtion is, when we should stop the method call
+         * When n is 5 then stop call the function
+         * 
          */
         if (n == 5) {
             // stopping another call
             return;
         }
 
-        // Until reach base condition, we will call this method again and again
+        /*
+         * Recursion call
+         * Until reach base condition, we will call this method again and again
+         */
         print(n + 1);
     }
 }
