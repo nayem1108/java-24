c824783d9002cd14d03b0e0a91646c3155ba75b5
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-17 22:27:59 +0600

    types of variable used in recursion

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -2,7 +2,8 @@ package Recursion;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibo(7));
+        int fib = fibo(50);
+        System.out.println(fib);
     }
 
     static int fibo(int n) {
@@ -10,6 +11,22 @@ public class Fibonacci {
         if (n < 2) {
             return n;
         }
+        // System.out.print((n - 1) + " " + (n - 2));
+        // System.out.println();
         return fibo(n - 1) + fibo(n - 2);
     }
+
+    /*
+     * types of variable in recursion
+     * 
+     * 1. Argument fin(int n) n is argument variable
+     * 
+     * 2. function return type static int😛 fibo(int n)
+     * here int😛 is return type of the methodn
+     * 
+     * 3. inside method body
+     * {
+     * some variable in here may be used
+     * }
+     */
 }
